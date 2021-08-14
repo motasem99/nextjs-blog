efb1a00833a2eac7e292533216439fa11a11cda2
mutasem <motasemimad10@gmail.com>
2021-08-14 15:07:27 +0300

    clear code

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import utilStyles from '../styles/utils.module.css';
-import styles from './layoutStyle.module.css';
+import styles from './Style.module.css';
 import Link from 'next/link';
 
 const name = 'mutasem kwaik';
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import Date from '../components/date';
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from '../components/Layout';
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
